9f2e06cdbf0f67dcdf45ddbbd324b33771089c12
salenctawer <salenctawer@gmail.com>
2022-07-04 21:25:16 +0500

    register and login was added

--- index.js
+++ index.js
@@ -1,29 +1,104 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+
+import userModel from "./models/User.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@cluster0.ma431.mongodb.net/notes?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB OK"))
+  .catch(() => console.log("DB error", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.end("Hello world111!");
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Неверный логин или пароль",
+      });
+    }
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+    const { passwordHash, ...userData } = user._doc;
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось авторизоваться",
+    });
+  }
 });
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400), json(errors.array());
+    }
 
-  const token = jwt.sign(
-    {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new userModel({
       email: req.body.email,
-      fullName: "Some Name",
-    },
-    "secret123"
-  );
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-  res.json({
-    success: true,
-    token,
-  });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
+  }
 });
 
 app.listen(4444, (err) => {
