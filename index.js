9cf5bec31a2720823cbe4c2944222697af1b40f0
salenctawer <salenctawer@gmail.com>
2022-07-05 17:40:22 +0500

    upload image was added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 import {
   loginValidation,
   registerValidation,
@@ -24,15 +25,33 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 app.post("/auth/login", loginValidation, login);
 app.post("/auth/register", registerValidation, register);
 app.get("/auth/me", checkAuth, getMe);
 
-app.delete("/notes/:id", checkAuth, remove);
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
 app.get("/notes", getAll);
 app.get("/notes/:id", getOne);
+app.delete("/notes/:id", checkAuth, remove);
 app.patch("/notes/:id", checkAuth, update);
 app.post("/notes", checkAuth, notesCreateValidation, create);
 
